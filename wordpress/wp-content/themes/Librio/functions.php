e7a4c7ea58537055e4029a2bfd2ff1cf555c66b2
James Wachira <jamomathenge@gmail.com>
2010-06-27 21:15:50 -0700

    -removing theme

--- wordpress/wp-content/themes/Librio/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-register_sidebar( array(
-	'before_widget' => '<div class="block">', 'after_widget' => '</div>',
-	'before_title' => '<h3>', 'after_title' => '</h3>' ));
-
-//GsL98DGtpo0W
-
-?>
