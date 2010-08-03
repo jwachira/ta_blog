8bea77649918a35eb9518392f0256012e26ba197
James Wachira <jamomathenge@gmail.com>
2010-08-03 11:51:37 -0700

    new thesis theme

--- wordpress/wp-content/themes/Curious/sidebar_advertisement.php
+++
@@ -1,4 +0,0 @@
-<a href="#"><img src="<?php bloginfo('template_url'); ?>/images/advertisement/thumb1.gif" alt="advertisement1"></img></a>
-<a href="http://www.corkdump.com"><img src="<?php bloginfo('template_url'); ?>/images/advertisement/thumb2.gif" alt="advertisement2"></img></a>
-<a href="#"><img src="<?php bloginfo('template_url'); ?>/images/advertisement/thumb3.gif" alt="advertisement3"></img></a>
-<a href="#"><img src="<?php bloginfo('template_url'); ?>/images/advertisement/thumb4.gif" alt="advertisement4"></img></a>
